287c1eb773878b0ca8d2a3c72ca37c7c3b864550
psikolele <serra.emanuele09@gmail.com>
2026-01-16 15:40:47 +0100

    fix: Bump to v1.2 and clean up footer JSX

--- App.tsx
+++ App.tsx
@@ -633,8 +633,8 @@ export default function App() {
 
             {/* Version Footer */}
             <div className="fixed bottom-2 right-4 z-40 text-[10px] text-gray-600 font-mono opacity-50 hover:opacity-100 transition-opacity">
-                v1.1 | Brand Profile Generator
+                v1.2 | Brand Profile Generator
             </div>
-        </div >
+        </div>
     );
 }
